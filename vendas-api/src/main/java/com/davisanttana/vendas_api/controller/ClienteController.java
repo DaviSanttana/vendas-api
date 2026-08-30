e3b20ecad6451812953c9512959b1982bb49bfcc
Davi Santtana <batatovisk0330@gmail.com>
2026-08-30 20:09:48 -0300

    fix: criacao do crud de clientes

--- vendas-api/src/main/java/com/davisanttana/vendas_api/controller/ClienteController.java
+++ vendas-api/src/main/java/com/davisanttana/vendas_api/controller/ClienteController.java
@@ -5,6 +5,7 @@ import com.davisanttana.vendas_api.model.Cliente;
 import com.davisanttana.vendas_api.repository.ClienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +26,37 @@ public class ClienteController {
     public Cliente criarClientes(@Valid @RequestBody Cliente cliente){
         return clienteRepository.save(cliente);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
+        return clienteRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id, @Valid @RequestBody Cliente clienteAtualizado) {
+        return clienteRepository.findById(id)
+                .map(clienteExistente -> {
+                    clienteExistente.setNome(clienteAtualizado.getNome());
+                    clienteExistente.setCpf(clienteAtualizado.getCpf());
+                    clienteExistente.setEmail(clienteAtualizado.getEmail());
+                    clienteExistente.setTelefone(clienteAtualizado.getTelefone());
+
+                    Cliente salvo = clienteRepository.save(clienteExistente);
+                    return ResponseEntity.ok(salvo);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id){
+        if (clienteRepository.existsById(id)){
+            clienteRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 
 }
