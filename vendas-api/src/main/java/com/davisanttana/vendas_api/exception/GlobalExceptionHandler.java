31b087e80aa06ab65639da224f9153d20801dc1a
Davi Santtana <batatovisk0330@gmail.com>
2026-08-31 01:34:44 -0300

    fix: atualização no exception para cpf duplicado

--- vendas-api/src/main/java/com/davisanttana/vendas_api/exception/GlobalExceptionHandler.java
+++ vendas-api/src/main/java/com/davisanttana/vendas_api/exception/GlobalExceptionHandler.java
@@ -50,4 +50,9 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
     }
+    public class CpfDuplicadoException extends RuntimeException {
+        public CpfDuplicadoException(String cpf) {
+            super("Já existe um cliente cadastrado com o CPF " + cpf);
+        }
+    }
 }
