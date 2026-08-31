81b29843301620ce3896a2fa96646a9db8aa0330
Davi Santtana <batatovisk0330@gmail.com>
2026-08-30 21:09:37 -0300

    fix: atualizacao do exception para campo cpf

--- vendas-api/src/main/java/com/davisanttana/vendas_api/exception/GlobalExceptionHandler.java
+++ vendas-api/src/main/java/com/davisanttana/vendas_api/exception/GlobalExceptionHandler.java
@@ -1,15 +1,20 @@
 package com.davisanttana.vendas_api.exception;
 
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,15 +22,32 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> lidarComErrosDeValidacao(MethodArgumentNotValidException ex){
-        Map<String, String> erros = new HashMap<>();
+    public Map<String, List<String>> lidarComErrosDeValidacao(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> erros = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.groupingBy(
+                        FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())
+                ));
 
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String nomeDoCampo = ((FieldError) error).getField();
-            String mensagemErro = error.getDefaultMessage();
-            erros.put(nomeDoCampo, mensagemErro);
-        });
+        ex.getBindingResult().getGlobalErrors().forEach(erroGlobal ->
+                erros.computeIfAbsent(erroGlobal.getObjectName(), k -> new ArrayList<>())
+                        .add(erroGlobal.getDefaultMessage())
+        );
 
         return erros;
+    }
+
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, String>> lidarComViolacaoDeIntegridade(DataIntegrityViolationException ex) {
+        Map<String, String> erro = new HashMap<>();
+
+        if (ex.getMessage().contains("CPF")) {
+            erro.put("erro", "Não foi possível realizar o cadastro. Verifique se os dados estão corretos.");
+        } else {
+            erro.put("erro", "Erro ao processar as informações enviadas.");
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
     }
 }
