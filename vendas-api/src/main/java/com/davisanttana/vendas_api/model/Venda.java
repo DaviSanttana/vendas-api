0915cd4442c4dbf8dcab4a4a5009d54b081ffd48
Davi Santtana <batatovisk0330@gmail.com>
2026-09-23 01:19:56 -0300

    feat: criando getters e setters da classe venda

--- vendas-api/src/main/java/com/davisanttana/vendas_api/model/Venda.java
+++ vendas-api/src/main/java/com/davisanttana/vendas_api/model/Venda.java
@@ -1,4 +1,73 @@
 package com.davisanttana.vendas_api.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
 public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @ManyToMany
+    @JoinTable(
+            name = "venda_produto",
+            joinColumns = @JoinColumn(name = "venda_id"),
+            inverseJoinColumns = @JoinColumn(name = "produto_id")
+    )
+    private List<Produto> produtos;
+
+    @Column(nullable = false)
+    private LocalDateTime dataVenda = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public LocalDateTime getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(LocalDateTime dateVenda) {
+        this.dataVenda = dateVenda;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
