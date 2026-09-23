853260172bac6cd443bbc872b2b31903157494fc
Davi Santtana <batatovisk0330@gmail.com>
2026-09-23 01:22:12 -0300

    feat: Atualizando interface VendasRepository

--- vendas-api/src/main/java/com/davisanttana/vendas_api/repository/VendasRepository.java
+++ vendas-api/src/main/java/com/davisanttana/vendas_api/repository/VendasRepository.java
@@ -1,4 +1,8 @@
 package com.davisanttana.vendas_api.repository;
 
-public interface VendasRepository {
+import com.davisanttana.vendas_api.model.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendasRepository extends JpaRepository<Venda, Long> {
+
 }
